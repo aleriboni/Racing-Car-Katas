ca352753d8f5b26e83f57ec1a45eaa7c2dfe69f4
aleriboni <alessandro.riboni@agilelab.it>
2023-06-15 12:31:39 +0200

    Add refactoring for TirePressureMonitoring exercise

--- Python/TirePressureMonitoringSystem/sensor.py
+++ Python/TirePressureMonitoringSystem/sensor.py
@@ -1,12 +1,13 @@
 import random
+from abc import ABC, abstractmethod
+
 
 class Sensor(object):
-
     # The reading of the pressure value from the sensor is simulated in this implementation.
     # Because the focus of the exercise is on the other class.
 
     _OFFSET = 16
-        
+
     def pop_next_pressure_psi_value(self):
         pressure_telemetry_value = self.sample_pressure()
         return Sensor._OFFSET + pressure_telemetry_value
@@ -16,3 +17,19 @@ class Sensor(object):
         # placeholder implementation that simulate a real sensor in a real tire
         pressure_telemetry_value = 6 * random.random() * random.random()
         return pressure_telemetry_value
+
+
+class SensorInterface(ABC):
+
+    @abstractmethod
+    def get_psi_pressure(self):
+        pass
+
+
+class PressureSensor(SensorInterface):
+
+    def __init__(self):
+        self.sensor = Sensor()
+
+    def get_psi_pressure(self):
+        return self.sensor.pop_next_pressure_psi_value()
